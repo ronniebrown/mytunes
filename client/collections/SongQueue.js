a093da986b15cb08cb7e47c2f2118d962e4bade0
Ronnie Brown <ronnievoice@gmail.com>
2015-05-27 18:49:35 -0700

    tests pass for songModel and SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,18 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
-  }
+  initialize: function() {
+    this.on( 'add', this.playFirst, this );
+    this.on( 'dequeue', this.dequeue, this );
+  },
 
+  playFirst: function(song) {
+    if (this.models.length === 1) {
+      this.models[0].play();
+    } 
+  },
+
+  dequeue: function(song) {
+    this.shift();
+  },
 });
