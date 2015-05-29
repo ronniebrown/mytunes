e8e61360daec930a44cba5e5855f710800954e91
Ronnie Brown <ronnievoice@gmail.com>
2015-05-29 08:31:35 -0700

    finished appModel

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -18,7 +18,7 @@ var AppModel = Backbone.Model.extend({
     }, this);
 
     params.library.on('enqueue', function(song) {
-      this.get('songQueue').add(song);
+    this.get('songQueue').add(song);
     }, this);
 
     this.get('songQueue').on('stop', function() {
